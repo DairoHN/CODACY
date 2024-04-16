b06ddb7424a416269d8c2b792a15d9eac9e3ab64
DairoHN <150400888+DairoHN@users.noreply.github.com>
2024-04-16 16:15:59 -0500

    Delete jpaprojections-main-20240415T233702Z-001/jpaprojections-main directory

--- jpaprojections-main-20240415T233702Z-001/jpaprojections-main/src/main/java/com/jaax/jpaprojections/projection/interfacebased/closed/LocalClosedView.java
+++
@@ -1,8 +0,0 @@
-package com.jaax.jpaprojections.projection.interfacebased.closed;
-
-public interface LocalClosedView {
-
-    String getName();
-    String getFloor();
-
-}
--- jpaprojections-main-20240415T233702Z-001/jpaprojections-main/src/main/java/com/jaax/jpaprojections/projection/interfacebased/closed/ProductClosedView.java
+++
@@ -1,7 +0,0 @@
-package com.jaax.jpaprojections.projection.interfacebased.closed;
-
-public interface ProductClosedView {
-    String getName();
-    String getBrand();
-    LocalClosedView getLocal();
-}
